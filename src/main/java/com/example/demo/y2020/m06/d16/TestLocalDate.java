7bb625354b01e6be5610f889e957e6130c1362f6
gaoruiting <gaoruiting@fantaike.ai>
2020-06-17 15:08:52 +0800

    :construction:  测试LocalDate和LocalDateTime

--- src/main/java/com/example/demo/y2020/m06/d16/TestLocalDate.java
+++ src/main/java/com/example/demo/y2020/m06/d16/TestLocalDate.java
@@ -5,11 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class TestLocalDate {
     public static void main(String[] args) {
-        LocalDate of = LocalDate.of(2020, 7, 1);
-        System.out.println(of);
-        String dataTime1 = "2020-06-16 11:00:00";
+        LocalDate date = LocalDate.parse("2020-07-01", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        System.out.println(date);
+        String dataTime1 = "2020-07-01 11:00:00";
         LocalDate parse = LocalDate.parse(dataTime1, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         System.out.println(parse);
-        System.out.println(parse.isAfter(of));
+        System.out.println(parse.isBefore(date));
     }
 }
