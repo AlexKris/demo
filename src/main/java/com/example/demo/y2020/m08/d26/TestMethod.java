c1d32c12c5a40da994d9966eb63b32441ac03c74
gaoruiting <gaoruiting@fantaike.ai>
2020-09-03 17:58:54 +0800

    :construction: 测试方法内操作对象，方法外是否有改变

---
+++ src/main/java/com/example/demo/y2020/m08/d26/TestMethod.java
@@ -0,0 +1,16 @@
+package com.example.demo.y2020.m08.d26;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMethod {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        testMethod(strings);
+        System.out.println(strings);
+    }
+
+    public static void testMethod(List<String> strings) {
+        strings.add("01");
+    }
+}
